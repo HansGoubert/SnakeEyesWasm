9bc75da32183f28ba24bce682c971a8cb576d997
Hans Goubert <hans.goubert@student.hogent.be>
2023-12-10 18:56:38 +0100

    test deploy

---
+++ src/Domain/Game.cs
@@ -0,0 +1,44 @@
+﻿namespace Domain;
+public class Game
+{
+
+    private Dice _dice1 = default!;
+    private Dice _dice2 = default!;
+    private readonly List<int> _highscores = new ();
+
+    public int Eye1 => _dice1.Dots;
+    public int Eye2 => _dice2.Dots;
+    public bool HasSnakeEyes => Eye1 == 1 && Eye2 == 1;
+    public IReadOnlyList<int> HighScores => _highscores.AsReadOnly();
+    public int Total { get; private set; } = 0;
+
+    public Game()
+    {
+        Initialize();
+        _highscores = new List<int>();
+    }
+
+    private void Initialize()
+    {
+        _dice1 = new Dice();
+        _dice2 = new Dice();
+    }
+    public void Play()
+    {
+        _dice1.Roll();
+        _dice2.Roll();
+        if (HasSnakeEyes)
+        {
+            _highscores.Add(Total);
+            Total = 0;
+        }
+        else
+        {
+			Total += Eye1 + Eye2;
+		}
+    }
+    public void Restart()
+    {
+        Initialize();
+    }
+}
